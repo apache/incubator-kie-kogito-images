radtriste/kogito-cloud

KOGITO-2374 Setup Maven repo url

scripts/update-maven-information.py
@@ -10,6 +10,8 @@
 import sys
 sys.dont_write_bytecode = True
 
+import common
+
 import xml.etree.ElementTree as ET
 import requests
 import subprocess as sp
@@ -17,9 +19,9 @@ import os
 import argparse
 from ruamel.yaml import YAML
 
-import common
+MAVEN_MODULE="kogito-maven/3.6.x"
 
-DEFAULT_REPO_URL = "https://repository.jboss.org/"
+DEFAULT_REPO_URL = "https://repository.jboss.org/nexus/content/groups/public/"
 DEFAULT_VERSION = "8.0.0-SNAPSHOT"
 DEFAULT_ARTIFACT_PATH = "org/kie/kogito"
 
@@ -42,10 +44,10 @@ def isSnapshotVersion(version):
 def getMetadataRoot(service):
     '''
     Get the root element from the maven-metadata
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: root object
     '''
-    metadataURL=service["repoUrl"]+"maven-metadata.xml"
+    metadataURL=service["repo_url"]+"maven-metadata.xml"
     mavenMetadata=requests.get(metadataURL)
     with open('maven-metadata.xml', 'wb') as f:
         f.write(mavenMetadata.content)
@@ -57,7 +59,7 @@ def getMetadataRoot(service):
 def getSnapshotVersion(service):
     '''
     parse the xml and finds the snapshotVersion
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: snapshotVersion string
     '''
     root=getMetadataRoot(service)
@@ -67,19 +69,19 @@ def getSnapshotVersion(service):
 def getRunnerURL(service):
     ''' 
     Creates the updated URL for runner.jar
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: url string
     '''
     finalVersion = service["version"]
     if isSnapshotVersion(finalVersion):
         finalVersion=getSnapshotVersion(service)
-    url = service["repoUrl"] + "{}-{}-runner.jar".format(service["name"], finalVersion)
+    url = service["repo_url"] + "{}-{}-runner.jar".format(service["name"], finalVersion)
     return url
 
 def getMD5(service):
     '''
     Fetches the md5 code for the latest runner.jar
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: runnerMD5 string
     '''
     runnerURL=getRunnerURL(service)
@@ -87,41 +89,41 @@ def getMD5(service):
     runnerMD5=sp.getoutput("curl -s  {}".format(runnerMD5URL))
     return runnerMD5
 
-def yaml_loader():
-    '''
-    default yaml Loader
-    :return: yaml object
-    '''
-    yaml = YAML()
-    yaml.preserve_quotes = True
-    yaml.width = 1024
-    yaml.indent(mapping=2, sequence=4, offset=2)
-    return yaml
-
 def update_artifacts(service,modulePath):
     '''
     Updates the module.yaml file of services with latest artifacts
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :param modulePath: relative file location of the module.yaml for the kogito service
     '''
 
     with open(modulePath) as module:
-        data=yaml_loader().load(module)
+        data=common.yaml_loader().load(module)
         data['artifacts'][0]['url']=getRunnerURL(service)
         data['artifacts'][0]['md5']=getMD5(service)
     with open(modulePath, 'w') as module:
-        yaml_loader().dump(data, module)
+        common.yaml_loader().dump(data, module)
 
+def update_test_apps_clone_repo(repo_url):
+    file = 'tests/test-apps/clone-repo.sh'
+    print('Updating file {}'.format(file))
+    os.system('sed -i \'s|^export MAVEN_REPO_URL=.*|export MAVEN_REPO_URL=\"{}\"|\' '.format(repo_url) + file)
+
+def update_maven_repo(repo_url):
+    common.update_env_in_all_modules("MAVEN_REPO_URL", repo_url)
+    update_test_apps_clone_repo(repo_url)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Update the snapshot url for kogito services.')
-    parser.add_argument('--repo-url', dest='repoUrl', default=DEFAULT_REPO_URL, help='Defines the url of the repository to extract the artifacts from, defaults to {}'.format(DEFAULT_REPO_URL))
+    parser = argparse.ArgumentParser(description='Update Maven information in repo from the given artifact url and version.')
+    parser.add_argument('--repo-url', dest='repo_url', default=DEFAULT_REPO_URL, help='Defines the url of the repository to extract the artifacts from, defaults to {}'.format(DEFAULT_REPO_URL))
     parser.add_argument('--version', dest='version', default=DEFAULT_VERSION, help='Defines the version of artifacts to retrieve from the repository url, defaults to {}'.format(DEFAULT_VERSION))
     args = parser.parse_args()
+
+    update_maven_repo(args.repo_url)
     
+    # Update Kogito Service modules
     for serviceName, modulePath in Modules.items():
         service = {
-            "repoUrl" : args.repoUrl + "{}/{}/{}/".format(DEFAULT_ARTIFACT_PATH, serviceName, args.version),
+            "repo_url" : args.repo_url + "{}/{}/{}/".format(DEFAULT_ARTIFACT_PATH, serviceName, args.version),
             "name" : serviceName,
             "version" : args.version
         }
@@ -130,4 +132,5 @@ if __name__ == "__main__":
         update_artifacts(service, moduleYamlFile)
         print("Successfully updated the artifacts for: ", serviceName)
     
-    common.update_kogito_version_in_modules(args.version)
+    # Need also to set the KOGITO_VERSION for artifacts to the given one
+    common.update_kogito_version_env_in_modules(args.version)

tests/test-apps/clone-repo.sh
@@ -2,10 +2,19 @@
 #
 # Clone the kogito-examples and edit the rules-quarkus-helloworld and dmn-quarkus-example for testing purposes
 
+SCRIPT_DIR=`pwd`
+MVN_MODULE="${SCRIPT_DIR}/../../modules/kogito-maven/3.6.x"
+
 # exit when any command fails
 set -e
 
-TEST_DIR=`pwd`
+# setup maven env
+export MAVEN_REPO_URL="https://repository.jboss.org/nexus/content/groups/public/"
+cp ${MVN_MODULE}/maven/settings.xml ${HOME}/.m2/settings.xml
+source ${MVN_MODULE}/added/configure-maven.sh
+configure
+
+# Clone examples
 cd /tmp
 rm -rf kogito-examples/
 git clone https://github.com/kiegroup/kogito-examples.git
@@ -29,7 +38,7 @@ rm -rf /tmp/kogito-examples/dmn-quarkus-example/pom.xml
 # by adding the application.properties file telling app to start on
 # port 10000, the purpose of this tests is make sure that the images
 # will ensure the use of the port 8080.
-cp ${TEST_DIR}/application.properties /tmp/kogito-examples/rules-quarkus-helloworld/src/main/resources/META-INF/
+cp ${SCRIPT_DIR}/application.properties /tmp/kogito-examples/rules-quarkus-helloworld/src/main/resources/META-INF/
 (echo ""; echo "server.port=10000") >> /tmp/kogito-examples/process-springboot-example/src/main/resources/application.properties
 
 git add --all  :/

apache/incubator-kie-kogito-images

scripts/update-maven-information.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #Script responsible for fetching the latest artifacts of kogito services and updating their module.yaml files
 #Should be run from root directory of the repository
-#Sample usage:  python3 scripts/update-artifacts.py
+#Sample usage:  python3 scripts/update-maven-information.py
 #
 #Dependencies
 # ruamel.yaml
@@ -10,6 +10,8 @@
 import sys
 sys.dont_write_bytecode = True
 
+import common
+
 import xml.etree.ElementTree as ET
 import requests
 import subprocess as sp
@@ -17,9 +19,9 @@ import os
 import argparse
 from ruamel.yaml import YAML
 
-import common
+MAVEN_MODULE="kogito-maven/3.6.x"
 
-DEFAULT_REPO_URL = "https://repository.jboss.org/"
+DEFAULT_REPO_URL = "https://repository.jboss.org/nexus/content/groups/public/"
 DEFAULT_VERSION = "8.0.0-SNAPSHOT"
 DEFAULT_ARTIFACT_PATH = "org/kie/kogito"
 
@@ -42,10 +44,10 @@ def isSnapshotVersion(version):
 def getMetadataRoot(service):
     '''
     Get the root element from the maven-metadata
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: root object
     '''
-    metadataURL=service["repoUrl"]+"maven-metadata.xml"
+    metadataURL=service["repo_url"]+"maven-metadata.xml"
     mavenMetadata=requests.get(metadataURL)
     with open('maven-metadata.xml', 'wb') as f:
         f.write(mavenMetadata.content)
@@ -57,7 +59,7 @@ def getMetadataRoot(service):
 def getSnapshotVersion(service):
     '''
     parse the xml and finds the snapshotVersion
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: snapshotVersion string
     '''
     root=getMetadataRoot(service)
@@ -67,19 +69,19 @@ def getSnapshotVersion(service):
 def getRunnerURL(service):
     ''' 
     Creates the updated URL for runner.jar
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: url string
     '''
     finalVersion = service["version"]
     if isSnapshotVersion(finalVersion):
         finalVersion=getSnapshotVersion(service)
-    url = service["repoUrl"] + "{}-{}-runner.jar".format(service["name"], finalVersion)
+    url = service["repo_url"] + "{}-{}-runner.jar".format(service["name"], finalVersion)
     return url
 
 def getMD5(service):
     '''
     Fetches the md5 code for the latest runner.jar
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :return: runnerMD5 string
     '''
     runnerURL=getRunnerURL(service)
@@ -87,41 +89,41 @@ def getMD5(service):
     runnerMD5=sp.getoutput("curl -s  {}".format(runnerMD5URL))
     return runnerMD5
 
-def yaml_loader():
-    '''
-    default yaml Loader
-    :return: yaml object
-    '''
-    yaml = YAML()
-    yaml.preserve_quotes = True
-    yaml.width = 1024
-    yaml.indent(mapping=2, sequence=4, offset=2)
-    return yaml
-
 def update_artifacts(service,modulePath):
     '''
     Updates the module.yaml file of services with latest artifacts
-    :param service: Service information (repoUrl, version, name)
+    :param service: Service information (repo_url, version, name)
     :param modulePath: relative file location of the module.yaml for the kogito service
     '''
 
     with open(modulePath) as module:
-        data=yaml_loader().load(module)
+        data=common.yaml_loader().load(module)
         data['artifacts'][0]['url']=getRunnerURL(service)
         data['artifacts'][0]['md5']=getMD5(service)
     with open(modulePath, 'w') as module:
-        yaml_loader().dump(data, module)
+        common.yaml_loader().dump(data, module)
 
+def update_test_apps_clone_repo(repo_url):
+    file = 'tests/test-apps/clone-repo.sh'
+    print('Updating file {}'.format(file))
+    os.system('sed -i \'s|^export JBOSS_MAVEN_REPO_URL=.*|export JBOSS_MAVEN_REPO_URL=\"{}\"|\' '.format(repo_url) + file)
+
+def update_maven_repo(repo_url):
+    common.update_env_in_all_modules("JBOSS_MAVEN_REPO_URL", repo_url)
+    update_test_apps_clone_repo(repo_url)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Update the snapshot url for kogito services.')
-    parser.add_argument('--repo-url', dest='repoUrl', default=DEFAULT_REPO_URL, help='Defines the url of the repository to extract the artifacts from, defaults to {}'.format(DEFAULT_REPO_URL))
+    parser = argparse.ArgumentParser(description='Update Maven information in repo from the given artifact url and version.')
+    parser.add_argument('--repo-url', dest='repo_url', default=DEFAULT_REPO_URL, help='Defines the url of the repository to extract the artifacts from, defaults to {}'.format(DEFAULT_REPO_URL))
     parser.add_argument('--version', dest='version', default=DEFAULT_VERSION, help='Defines the version of artifacts to retrieve from the repository url, defaults to {}'.format(DEFAULT_VERSION))
     args = parser.parse_args()
+
+    update_maven_repo(args.repo_url)
     
+    # Update Kogito Service modules
     for serviceName, modulePath in Modules.items():
         service = {
-            "repoUrl" : args.repoUrl + "{}/{}/{}/".format(DEFAULT_ARTIFACT_PATH, serviceName, args.version),
+            "repo_url" : args.repo_url + "{}/{}/{}/".format(DEFAULT_ARTIFACT_PATH, serviceName, args.version),
             "name" : serviceName,
             "version" : args.version
         }
@@ -130,4 +132,5 @@ if __name__ == "__main__":
         update_artifacts(service, moduleYamlFile)
         print("Successfully updated the artifacts for: ", serviceName)
     
-    common.update_kogito_version_in_modules(args.version)
+    # Need also to set the KOGITO_VERSION for artifacts to the given one
+    common.update_kogito_version_env_in_modules(args.version)

tests/test-apps/clone-repo.sh
@@ -2,10 +2,19 @@
 #
 # Clone the kogito-examples and edit the rules-quarkus-helloworld and dmn-quarkus-example for testing purposes
 
+SCRIPT_DIR=`pwd`
+MVN_MODULE="${SCRIPT_DIR}/../../modules/kogito-maven/3.6.x"
+
 # exit when any command fails
 set -e
 
-TEST_DIR=`pwd`
+# setup maven env
+export JBOSS_MAVEN_REPO_URL="https://repository.jboss.org/nexus/content/groups/public/"
+cp ${MVN_MODULE}/maven/settings.xml ${HOME}/.m2/settings.xml
+source ${MVN_MODULE}/added/configure-maven.sh
+configure
+
+# Clone examples
 cd /tmp
 rm -rf kogito-examples/
 git clone https://github.com/kiegroup/kogito-examples.git
@@ -29,8 +38,10 @@ rm -rf /tmp/kogito-examples/dmn-quarkus-example/pom.xml
 # by adding the application.properties file telling app to start on
 # port 10000, the purpose of this tests is make sure that the images
 # will ensure the use of the port 8080.
-cp ${TEST_DIR}/application.properties /tmp/kogito-examples/rules-quarkus-helloworld/src/main/resources/META-INF/
+cp ${SCRIPT_DIR}/application.properties /tmp/kogito-examples/rules-quarkus-helloworld/src/main/resources/META-INF/
 (echo ""; echo "server.port=10000") >> /tmp/kogito-examples/process-springboot-example/src/main/resources/application.properties
 
 git add --all  :/
 git commit -am "test"
+
+rm ${HOME}/.m2/settings.xml
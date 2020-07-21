8efcfed2b1d5d89f0934514d9268119ca7ee45f2
jake <jacopo.r00ta@gmail.com>
2020-07-21 16:07:59 +0200

    remove trusty infinispan dependencies not needed anymore

--- modules/kogito-trusty/added/kogito-app-launch.sh
+++ modules/kogito-trusty/added/kogito-app-launch.sh
@@ -14,13 +14,12 @@ fi
 # Configuration scripts
 # Any configuration script that needs to run on image startup must be added here.
 CONFIGURE_SCRIPTS=(
-  ${KOGITO_HOME}/launch/kogito-infinispan-properties.sh
   ${KOGITO_HOME}/launch/kogito-trusty.sh
 )
 source ${KOGITO_HOME}/launch/configure.sh
 #############################################
 
-exec java ${SHOW_JVM_SETTINGS} ${JAVA_OPTIONS} ${INFINISPAN_PROPERTIES} ${KOGITO_TRUSTY_PROPS} \
+exec java ${SHOW_JVM_SETTINGS} ${JAVA_OPTIONS} ${KOGITO_TRUSTY_PROPS} \
         -Djava.library.path=$KOGITO_HOME/lib \
         -Dquarkus.http.host=0.0.0.0 \
         -jar $KOGITO_HOME/bin/kogito-trusty-runner.jar

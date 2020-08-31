3d8f519371a686f371f95b0105cf500748490d05
jake <jacopo.r00ta@gmail.com>
2020-08-31 21:04:46 +0200

    refactoring

--- modules/kogito-trusty/added/launch/kogito-trusty.sh
+++ modules/kogito-trusty/added/launch/kogito-trusty.sh
@@ -3,11 +3,13 @@
 
 function prepareEnv() {
     # keep it on alphabetical order
+    unset EXPLAINABILITY_ENABLED
     unset HTTP_PORT
 }
 
 function configure() {
     configure_trusty_http_port
+    enable_explainability
 }
 
 function configure_trusty_http_port {
@@ -15,3 +17,12 @@ function configure_trusty_http_port {
     KOGITO_TRUSTY_PROPS="${KOGITO_TRUSTY_PROPS} -Dquarkus.http.port=${httpPort}"
 }
 
+function enable_explainability {
+    local explainabilityEnabled=${EXPLAINABILITY_ENABLED^^}
+    if [ "${explainabilityEnabled}" = "FALSE" ] ; then 
+      explainabilityEnabled="false"
+    else
+      explainabilityEnabled="true"
+    fi
+    KOGITO_TRUSTY_PROPS="${KOGITO_TRUSTY_PROPS} -Dtrusty.explainability.enabled=${explainabilityEnabled}"
+}

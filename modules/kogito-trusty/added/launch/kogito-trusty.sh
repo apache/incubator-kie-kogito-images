41310dffb6d27f15ed3ceae85cc72e6039f03318
Jacopo Rota <jacopo.r00ta@gmail.com>
2020-09-02 19:49:51 +0200

    Update kogito-trusty.sh

--- modules/kogito-trusty/added/launch/kogito-trusty.sh
+++ modules/kogito-trusty/added/launch/kogito-trusty.sh
@@ -18,11 +18,9 @@ function configure_trusty_http_port {
 }
 
 function enable_explainability {
-    local explainabilityEnabled=${EXPLAINABILITY_ENABLED^^}
-    if [ "${explainabilityEnabled}" = "FALSE" ] ; then 
-      explainabilityEnabled="false"
-    else
-      explainabilityEnabled="true"
+    local explainabilityEnabled=${EXPLAINABILITY_ENABLED:"true"}
+    if [ "${explainabilityEnabled^^}" = "TRUE" ] ; then 
+          explainabilityEnabled="true"
     fi
     KOGITO_TRUSTY_PROPS="${KOGITO_TRUSTY_PROPS} -Dtrusty.explainability.enabled=${explainabilityEnabled}"
 }

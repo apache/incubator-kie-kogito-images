adc99c33a4ef274ec4f8e6a97dc7271394ab2353
Jenkins CI <jenkins@rhba-jenkins.rhev-ci-vms.eng.rdu2.redhat.com>
2020-09-17 05:53:05 -0400

    Update snapshot version to 0.15.1-snapshot

--- scripts/update-maven-artifacts.py
+++ scripts/update-maven-artifacts.py
@@ -21,7 +21,7 @@ import argparse
 DEFAULT_REPO_URL = "https://repository.jboss.org/nexus/content/groups/public/"
 KOGITO_ARTIFACT_PATH = "org/kie/kogito"
 
-ARTIFACTS_VERSION="0.15.0"
+ARTIFACTS_VERSION="0.15.1-SNAPSHOT"
 
 Modules = {
     #service-name: module-name(directory in which module's module.yaml file is present)

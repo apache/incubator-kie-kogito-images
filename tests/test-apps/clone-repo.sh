9a3d9a741b582a8e08e83e76739ca0e9c7929298
Tristan Radisson <tristan.radisson@gmail.com>
2021-01-26 14:22:46 +0100

    Set version 1.2.0-snapshot (#333)

--- tests/test-apps/clone-repo.sh
+++ tests/test-apps/clone-repo.sh
@@ -27,7 +27,7 @@ rm -rf kogito-examples/
 git clone https://github.com/kiegroup/kogito-examples.git
 cd kogito-examples/
 git fetch origin --tags
-git checkout master
+git checkout -b 1.2.x origin/1.2.x
 
 # make a new copy of rules-quarkus-helloworld for native tests
 cp -rv  /tmp/kogito-examples/rules-quarkus-helloworld/ /tmp/kogito-examples/rules-quarkus-helloworld-native/

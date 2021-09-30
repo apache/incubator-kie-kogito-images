443d67e758621267911c8606f5d40e1bc7685162
spolti <fspolti@redhat.com>
2021-09-30 14:18:34 -0300

    rename JAVA_OPTS to DYNAMIC_RESOURCES_OPTS
    Signed-off-by: spolti <fspolti@redhat.com>

--- modules/kogito-builder/added/kogito-app-launch.sh
+++ modules/kogito-builder/added/kogito-app-launch.sh
@@ -23,7 +23,7 @@ source "${KOGITO_HOME}"/launch/configure.sh
 
 runtime_type=$(get_runtime_type)
 
-JAVA_OPTS="$(${JBOSS_CONTAINER_JAVA_JVM_MODULE}/java-default-options) $(${JBOSS_CONTAINER_JAVA_JVM_MODULE}/debug-options)"
+DYNAMIC_RESOURCES_OPTS="$(${JBOSS_CONTAINER_JAVA_JVM_MODULE}/java-default-options) $(${JBOSS_CONTAINER_JAVA_JVM_MODULE}/debug-options)"
 
 #############################################
 
@@ -31,18 +31,18 @@ case ${runtime_type} in
     "quarkus")  
         if [ "${NATIVE^^}" == "TRUE" ]; then
             # shellcheck disable=SC2086
-            exec "${KOGITO_HOME}"/bin/*-runner ${JAVA_OPTS} ${JAVA_OPTIONS} ${KOGITO_QUARKUS_S2I_PROPS} \
+            exec "${KOGITO_HOME}"/bin/*-runner ${DYNAMIC_RESOURCES_OPTS} ${JAVA_OPTIONS} ${KOGITO_QUARKUS_S2I_PROPS} \
                 -Dquarkus.http.host=0.0.0.0 -Djava.library.path="${KOGITO_HOME}"/ssl-libs \
                 -Dquarkus.http.port=8080 \
                 -Djavax.net.ssl.trustStore="${KOGITO_HOME}"/cacerts
         else
         # shellcheck disable=SC2086
-            exec java ${JAVA_OPTS} ${JAVA_OPTIONS} ${KOGITO_QUARKUS_S2I_PROPS} -Dquarkus.http.host=0.0.0.0 \
+            exec java ${DYNAMIC_RESOURCES_OPTS} ${JAVA_OPTIONS} ${KOGITO_QUARKUS_S2I_PROPS} -Dquarkus.http.host=0.0.0.0 \
                 -Dquarkus.http.port=8080 -jar "${KOGITO_HOME}"/bin/*.jar
         fi
     ;;
     "springboot") # shellcheck disable=SC2086
-        exec java ${JAVA_OPTS} ${JAVA_OPTIONS} ${KOGITO_SPRINGBOOT_S2I_PROPS} \
+        exec java ${DYNAMIC_RESOURCES_OPTS} ${JAVA_OPTIONS} ${KOGITO_SPRINGBOOT_S2I_PROPS} \
             -Dserver.address=0.0.0.0 -Dserver.port=8080 -jar "${KOGITO_HOME}"/bin/*.jar
     ;;
     *)

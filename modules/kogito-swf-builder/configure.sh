2a15a31df215fd8d536b1bb935f9c02f6ac977ac
desmax74 <mdessi@redhat.com>
2022-10-14 12:09:50 +0200

    [KOGITO-7779] Produce a BASE Kogito Serverless Workflow Builder Image
    Signed-off-by: desmax74 <mdessi@redhat.com>

--- modules/kogito-swf-builder/configure.sh
+++ modules/kogito-swf-builder/configure.sh
@@ -4,7 +4,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ADDED_DIR="${SCRIPT_DIR}"/added
 
-cp -v "${ADDED_DIR}"/build-app.sh "${KOGITO_HOME}"/launch
-
 chown -R 1001:0 "${KOGITO_HOME}"
 chmod -R ug+rwX "${KOGITO_HOME}"
--- modules/kogito-swf-builder/install.sh
+++ modules/kogito-swf-builder/install.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
+script_dir_path="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+resources_path="$1"
+quarkus_create_args=${QUARKUS_CREATE_ARGS}
+if [ ! -z "${resources_path}" ]; then
+  # quarkus_create_args="${quarkus_create_args} -DnoCode" # Not sure if we want to force the `noCode`
+  resources_path="$(realpath "${resources_path}")"
+fi
+
+# Call the configure-maven here
+source "${KOGITO_HOME}"/launch/configure-maven.sh
+configure
+
 cd "${KOGITO_HOME}"
 
-# Create scaffold project
-export PROJECT_GROUP_ID='org.acme'
-export PROJECT_ARTIFACT_ID='serverless-workflow-project'
-export PROJECT_VERSION='1.0.0-snapshot'
-export QUARKUS_EXTENSIONS='quarkus-kubernetes,kogito-quarkus-serverless-workflow,kogito-addons-quarkus-knative-eventing'
+set -x
+"${MAVEN_HOME}"/bin/mvn -U -B -s "${MAVEN_SETTINGS_PATH}" \
+io.quarkus.platform:quarkus-maven-plugin:"${QUARKUS_VERSION}":create ${quarkus_create_args} \
+-DprojectGroupId="${PROJECT_GROUP_ID}" \
+-DprojectArtifactId="${PROJECT_ARTIFACT_ID}" \
+-DprojectVersionId="${PROJECT_VERSION}" \
+-DplatformVersion="${QUARKUS_VERSION}" \
+-Dextensions="${QUARKUS_EXTENSIONS:-quarkus-kubernetes,kogito-quarkus-serverless-workflow,kogito-addons-quarkus-knative-eventing}"
 
-"${KOGITO_HOME}"/launch/build-app.sh
+cd "${PROJECT_ARTIFACT_ID}"
+
+# Copy resources if exists
+if [ ! -z "${resources_path}" ]; then
+  if [ -d "${resources_path}" ]; then
+    cp -rv "${resources_path}"/* src/main/resources/
+  else
+    cp -rv "${resources_path}" src/main/resources/
+  fi
+fi
+
+"${MAVEN_HOME}"/bin/mvn ${MAVEN_ARGS_APPEND} -U -B clean install -DskipTests -s "${MAVEN_SETTINGS_PATH}" -Dquarkus.container-image.build=false
+set +x

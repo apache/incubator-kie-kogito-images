7b8a83b70e6030702a834665c68e382cdb7b4731
Tristan Radisson <tristan.radisson@gmail.com>
2022-10-31 18:20:24 +0100

    Modify the way kogito-swf-builder is built
    That way, the scaffold build maven configuration is using the one from the node as we cannot set correct maven values for build in cekit ...
    
    (cherry picked from commit c857809a9dbc14c5c7d7dbe43eb8df47a86cb4f4)

--- modules/kogito-swf-builder/added/add-extension.sh
+++ modules/kogito-swf-builder/added/add-extension.sh
@@ -5,10 +5,10 @@ script_dir_path="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 extensions="$1"
 
 # Call the configure-maven here
-source "${KOGITO_HOME}"/launch/configure-maven.sh
+source "${script_dir_path}"/configure-maven.sh
 configure
 
-source "${KOGITO_HOME}"/launch/logging.sh
+source "${script_dir_path}"/logging.sh
 
 if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     set -x
@@ -16,7 +16,7 @@ if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     printenv
 fi
 
-cd "${KOGITO_HOME}/${PROJECT_ARTIFACT_ID}"
+cd "${PROJECT_ARTIFACT_ID}"
 
 "${MAVEN_HOME}"/bin/mvn -U -B -s "${MAVEN_SETTINGS_PATH}" \
 io.quarkus.platform:quarkus-maven-plugin:"${QUARKUS_VERSION}":add-extension ${QUARKUS_ADD_EXTENSION_ARGS}\

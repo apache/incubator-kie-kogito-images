283152a2cbaf28c1e05ab098eede0b6ac64a7fb9
Ricardo Zanini <zanini@redhat.com>
2023-01-18 18:08:28 -0300

    Add behave tests for running the app in devmode
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- modules/kogito-swf-builder/added/run-app-devmode.sh
+++ modules/kogito-swf-builder/added/run-app-devmode.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
+source "${KOGITO_HOME}"/launch/logging.sh
+
+if [ "${SCRIPT_DEBUG}" = "true" ] ; then
+    set -x
+    export MAVEN_ARGS_APPEND="${MAVEN_ARGS_APPEND} -X --batch-mode" 
+    log_info "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
+    printenv
+fi
+
+# Call the configure-maven here
+source "${KOGITO_HOME}"/launch/configure-maven.sh
+configure
+
 cd "${PROJECT_ARTIFACT_ID}"
-"${MAVEN_HOME}"/bin/mvn ${MAVEN_ARGS_APPEND} -U -B clean compile quarkus:dev -Dquarkus.http.host=0.0.0.0
+
+if [ ! -z "${QUARKUS_EXTENSIONS}" ]; then
+  ${KOGITO_HOME}/launch/add-extension.sh "${QUARKUS_EXTENSIONS}"
+fi
+
+# auto configure JVM settings
+source "${KOGITO_HOME}"/launch/jvm-settings.sh
+
+"${MAVEN_HOME}"/bin/mvn ${MAVEN_ARGS_APPEND} -U -B clean compile -DskipTests -s "${MAVEN_SETTINGS_PATH}" quarkus:dev -Dquarkus.http.host=0.0.0.0

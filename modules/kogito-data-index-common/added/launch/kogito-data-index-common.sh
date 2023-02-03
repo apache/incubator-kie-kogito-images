4774c14f45619685a4ac8df8ae02fe92ce94bffc
nmirasch <neus.miras@gmail.com>
2023-02-03 17:11:47 +0100

    changed log_info message when unvalid value passed

--- modules/kogito-data-index-common/added/launch/kogito-data-index-common.sh
+++ modules/kogito-data-index-common/added/launch/kogito-data-index-common.sh
@@ -14,7 +14,7 @@ function configure_data_index_quarkus_profile() {
     local quarkusProfile =${KOGITO_DATA_INDEX_QUARKUS_PROFILE}
     if [ "${quarkusProfile}x" != "x" ]; then
         if [[ "${quarkusProfile}" != "kafka-events-support"  &&  "${quarkusProfile}" != "http-events-support" ]]; then
-            log_info "Data Index Quarkus profile ${quarkusProfile} undefined. Replacing by the default: `kafka-events-support`"
+            log_info "Data Index Quarkus profile ${quarkusProfile} is not valid. Replacing it by the default: `kafka-events-support`"
             quarkusProfile="kafka-events-support"
         fi
     else

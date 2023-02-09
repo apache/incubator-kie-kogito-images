76047d87ff4eba30184bbabf76e564c7a5827926
nmirasch <neus.miras@gmail.com>
2023-02-09 15:21:05 +0100

    Only define a default value for KOGITO_DATA_INDEX_QUARKUS_PROFILE, no checking if it's empty

--- modules/kogito-data-index-common/added/launch/kogito-data-index-common.sh
+++ modules/kogito-data-index-common/added/launch/kogito-data-index-common.sh
@@ -12,9 +12,5 @@ function configure() {
 
 function configure_data_index_quarkus_profile() {
     local quarkusProfile =${KOGITO_DATA_INDEX_QUARKUS_PROFILE}
-    if [ "${quarkusProfile}x" == "x" ]; then
-        log_info "Applying default quarkus.profile=kafka-events-support"
-        quarkusProfile="kafka-events-support"
-    fi
     KOGITO_DATA_INDEX_PROPS="${KOGITO_DATA_INDEX_PROPS} -Dquarkus.profile=${quarkusProfile}"
 }

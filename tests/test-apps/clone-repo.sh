935dda7e3b1fb60dca242eee967a86df3bf0e934
Tristan Radisson <tradisso@redhat.com>
2023-05-31 20:49:02 +0200

    KOGITO-9259 CI: Update nodes to rhel8 (#1556)
    cherry-pick: https://github.com/kiegroup/kogito-images/pull/1552

--- tests/test-apps/clone-repo.sh
+++ tests/test-apps/clone-repo.sh
@@ -4,9 +4,9 @@
 
 SCRIPT_DIR=`pwd`
 MVN_MODULE="${SCRIPT_DIR}/../../modules/kogito-maven/3.6.x"
-CONTAINER_ENGINE="docker"
+CONTAINER_ENGINE=${CONTAINER_ENGINE:-"docker"}
 MAVEN_OPTIONS="-DskipTests -U"
-MAVEN_QUARKUS_NATIVE_CONTAINER_BUILD_ARGS="-Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=${CONTAINER_ENGINE}"
+MAVEN_QUARKUS_NATIVE_CONTAINER_BUILD_ARGS=${MAVEN_QUARKUS_NATIVE_CONTAINER_BUILD_ARGS:-"-Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=${CONTAINER_ENGINE}"}
 # MAVEN_IGNORE_SELF_SIGNED_CERTIFICATE=true
 
 NATIVE_BUILD=$1

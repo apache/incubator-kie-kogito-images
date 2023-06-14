ea4b617f6c856315583dc775c0395968b89185e1
Davide Salerno <dsalerno@redhat.com>
2023-06-14 09:59:23 +0200

    Changing artifacts section use name instead of path
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- modules/kogito-swf/devmode/runtime/osl/configure.sh
+++ modules/kogito-swf/devmode/runtime/osl/configure.sh
@@ -9,8 +9,8 @@ LAUNCH_DIR="${KOGITO_HOME}"/launch
 cp -v "${ADDED_DIR}"/* "${LAUNCH_DIR}"
 
 # Unzip Quarkus app and Maven repository
-unzip "${SOURCES_DIR}"/kogito-swf-quarkus-app.zip -d "${KOGITO_HOME}"
-unzip "${SOURCES_DIR}"/kogito-swf-maven-repo.zip -d "${KOGITO_HOME}"/.m2/repository
+unzip "${SOURCES_DIR}"/kogito-devmode-quarkus-app-image-build.zip -d "${KOGITO_HOME}"
+unzip "${SOURCES_DIR}"/kogito-devmode-maven-repository-image-build.zip -d "${KOGITO_HOME}"/.m2/repository
 
 chown -R 1001:0 "${KOGITO_HOME}"
 chmod -R ug+rwX "${KOGITO_HOME}"

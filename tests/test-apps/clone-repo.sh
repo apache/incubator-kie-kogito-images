d3ec51e903599dbd5ae2f4f98e23c1928b17867d
Jenkins CI <noreply@redhat.com>
2023-07-24 03:57:54 -0400

    [1.42.x] Update version to 1.42.0-snapshot

--- tests/test-apps/clone-repo.sh
+++ tests/test-apps/clone-repo.sh
@@ -40,7 +40,7 @@ git clone https://github.com/kiegroup/kogito-examples.git
 cd kogito-examples/
 git fetch origin
 git fetch origin --tags
-git checkout -b nightly-main
+git checkout -b nightly-1.42.x
 
 # make a new copy of rules-quarkus-helloworld for native tests
 cp -rv  /tmp/kogito-examples/kogito-quarkus-examples/rules-quarkus-helloworld/ /tmp/kogito-examples/kogito-quarkus-examples/rules-quarkus-helloworld-native/

5ba701be65697017b14a5954908a6c59a0bdc44d
Tristan Radisson <tristan.radisson@gmail.com>
2023-07-28 16:06:18 +0200

    Update scripts/logic/test/shell_test.sh
    Co-authored-by: Marián Macik <macik.marian@gmail.com>

--- scripts/logic/test/shell_test.sh
+++ scripts/logic/test/shell_test.sh
@@ -6,5 +6,5 @@ source ${script_dir_path}/env_test.sh $@
 echo "---- Pulling image ${image_full_tag} ----"
 docker pull ${image_full_tag}
 
-echo "---- Run behave test for image ${image_id} ----"
+echo "---- Run jBang test for image ${image_id} ----"
 tests/shell/run.sh ${community_image_id} ${image_full_tag}

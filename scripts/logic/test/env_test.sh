6482db6b8985cf3089764633df4b833fd9fe919d
Tristan Radisson <tradisso@redhat.com>
2023-07-31 16:26:24 +0200

    Prod test script: Allow to use community images (#1657)

--- scripts/logic/test/env_test.sh
+++ scripts/logic/test/env_test.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-declare -A corresponding_community_image_id
-corresponding_community_image_id['logic-data-index-ephemeral-rhel8']='kogito-data-index-ephemeral'
-
 export image_id=$1
-export image_sha=$2
+export image_full_tag=$2
 
-export image_registry=brew.registry.redhat.io/rh-osbs
-export image_serverless_namespace=openshift-serverless-1-tech-preview
-export image_name=${image_serverless_namespace}-${image_id}@sha256
-export image_full_tag=${image_registry}/${image_name}:${image_sha}
+export image_full_name=${image_full_tag%%:*}
+export image_registry_name=${image_full_name%/*}
+export image_name=${image_full_name##*/}
+export image_registry=${image_registry_name%/*}
+export image_namespace=${image_registry_name##*/}
+
 export image_descriptor_filename=${image_id}-image.yaml
 
 export community_image_id=${image_id/logic-/kogito-}
